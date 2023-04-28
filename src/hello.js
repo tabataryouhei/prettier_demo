3c690cd6d112bfb8688a3fff2cc20391bdc2d0b7
Yuta Kotake <yuta.kotake@ipg.co.jp>
2023-04-28 13:18:10 +0900

    first commit

---
+++ src/hello.js
@@ -0,0 +1,16 @@
+const func1 = (x) => {
+  console.log("hello world1");
+  console.log("hello world2");
+};
+
+const func2 = (x) => {
+  console.log("hello world1");
+  console.log("hello world2");
+  console.log("hello world3");
+};
+
+const func3 = (x) => {
+  console.log("hello world1");
+  console.log("hello world2");
+  console.log("hello world3");
+};
